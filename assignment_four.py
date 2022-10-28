40b9c43a12905f615543790ca4851f46c2c3e699
ziansin <pilchza@ssfs.org>
2022-10-28 14:05:52 -0400

    Fixed some issues and now the dealer can draw more cards

--- assignment_four.py
+++ assignment_four.py
@@ -33,31 +33,39 @@ def main():
     second = get_card()
     user_tot = first + second
     print("You drew the", first, "and the", second)
-    print("Your total is:", user_tot)
+    print("Your total is now:", user_tot)
     print("")
     y_or_n = str(input("Draw another card? "))
     if y_or_n == "y":
         third = get_card()
         user_tot = first + second + third
-        print("Your total is", user_tot)
+        print("Your total is now", user_tot)
+        if user_tot == 21:
+            print("Blackjack")
         if user_tot < 21:
             y_or_n_second = str(input("Draw another card? "))
             if y_or_n_second == "y":
                 fourth = get_card()
                 user_tot = first + second + third + fourth
-                print("Your total is", user_tot)
+                print("Your total is now", user_tot)
+                if user_tot == 21:
+                    print("Blackjack")
                 if user_tot < 21:
                     y_or_n_third = str(input("Draw another card? "))
                     if y_or_n_third == "y":
                         fifth = get_card()
                         user_tot = first + second + third + fourth + fifth
-                        print("Your total is", user_tot)
+                        print("Your total is now", user_tot)
+                        if user_tot == 21:
+                            print("Blackjack")
                         if user_tot < 21:
                             y_or_n_fourth = str(input("Draw another card? "))
                             if y_or_n_fourth == "y":
                                 sixth = get_card()
                                 user_tot = first + second + third + fourth + fifth + sixth
-                                print("Your total is", user_tot)
+                                print("Your total is now", user_tot)
+                                if user_tot == 21:
+                                    print("Blackjack")
                             elif y_or_n_third == "n":
                                 print("Your total is", user_tot)
                     elif y_or_n_third == "n":
@@ -70,19 +78,40 @@ def main():
     dealer_first = get_card()
     dealer_second = get_card()
     dealer_tot = dealer_first + dealer_second
+    print("The dealer drew the", dealer_first, "and the", dealer_second)
+    print("The dealer's total is", dealer_tot)
     if dealer_tot < 16:
         dealer_third = get_card()
         dealer_tot = dealer_first + dealer_second + dealer_third
-        if dealer_tot < 12:
+        print("")
+        print("The dealer drew the", dealer_third)
+        print("The dealer's total is", dealer_tot)
+        if dealer_tot == 21:
+            print("Blackjack")
+        if dealer_tot < 16:
             dealer_fourth = get_card()
             dealer_tot = dealer_first + dealer_second + dealer_third + dealer_fourth
-            print("The dealer has the", dealer_first, "the", dealer_second, "the", dealer_third, "and the", dealer_fourth)
-        else:
-            print("The dealer has the", dealer_first, "the", dealer_second, "and the", dealer_third)
+            print("")
+            print("The dealer drew the", dealer_fourth)
+            print("The dealer's total is", dealer_tot)
+            if dealer_tot == 21:
+                print("Blackjack")
+            if dealer_tot < 16:
+                dealer_fifth = get_card()
+                dealer_tot = dealer_first + dealer_second + dealer_third + dealer_fourth + dealer_fifth
+                print("")
+                print("The dealer drew the", dealer_fifth)
+                print("The dealer's total is", dealer_tot)
+                if dealer_tot == 21:
+                    print("Blackjack")
+            elif dealer_tot > 21:
+                print("Bust")
+        elif dealer_tot > 21:
+            print("Bust")
     else:
-        print("The dealer has the", dealer_first, "and the", dealer_second)
-    print("The dealer's total is", dealer_tot)
+        print("He did not draw any more cards")
     print("")
+    print(user_tot, "-", dealer_tot)
     (get_winner(user_tot, dealer_tot))
 
 main()
