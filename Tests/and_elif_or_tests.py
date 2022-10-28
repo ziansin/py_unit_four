5a124928451b379117e7a503fa91b2a4b7ccf097
ziansin <pilchza@ssfs.org>
2022-10-28 09:21:34 -0400

    Working unit test

--- Tests/and_elif_or_tests.py
+++ Tests/and_elif_or_tests.py
@@ -30,14 +30,12 @@ class MyTestCase(unittest.TestCase):
     def test_points_6(self):
         self.assertEqual(2, points(85, False))
 
-"""
 
     def test_rps(self):
         self.assertEqual("You win!", who_wins(3, 2))
         self.assertEqual("It is a tie", who_wins(1, 1))
 
         # Put the rest of the rps tests below this line. There should be 9 in total.
-"""
 
 if __name__ == '__main__':
     unittest.main()
